eff4775ad4745ff914221575a542fc2c550f018f
soft <1579571282@qq.com>
2022-11-15 11:53:43 +0800

    statistics微服务基调完成
    可以在里面写统计分析了

---
+++ service/service-statistics/src/main/java/cn/neud/neusurvey/statistics/controller/StatisticsUserController.java
@@ -0,0 +1,33 @@
+package cn.neud.neusurvey.statistics.controller;
+
+import cn.neud.common.utils.Result;
+import cn.neud.neusurvey.user.client.SurveyFeignClient;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("statistics/respondent")
+@Api(tags="user_group")                           // ?
+public class StatisticsUserController {
+    @Resource
+    SurveyFeignClient surveyFeignClient;
+
+    @GetMapping("/{id}")
+    @ApiOperation("答者统计数据")
+    @RequiresPermissions("user:usergroup:info")
+    public Result respondentStatistic(@PathVariable("id") String id) {
+        Result result = new Result();
+
+       result = surveyFeignClient.respondentStatistic(id);
+
+        return result;
+    }
+
+}
